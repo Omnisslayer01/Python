ff32711cb88dfff21015f2e6059869bfd6450240
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2025-08-31 09:15:21 +0530

    added Hour:Min:Sec counter with single line interface \r

--- Countdown_timer/countdown.py
+++ Countdown_timer/countdown.py
@@ -1,9 +1,14 @@
 import time
+import pygame
+import msvcrt
 
 userinput=int(input("Enter the time:"))
 for i in range(userinput,0,-1):
-    print(i)
+    second=i%60
+    minute=int(i/60)%60
+    hour=int(i/3600)%24
+
+    print(f"{hour:02}:{minute:02}:{second:02}",end="\r")
     time.sleep(1)
 
 print("TIMES UP!")
-    

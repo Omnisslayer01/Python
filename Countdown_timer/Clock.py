38f9b096421395830ac07ac9b0a1f6238dd9cb0a
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2025-09-27 21:10:02 +0530

    spaced code equally

--- Countdown_timer/Clock.py
+++ Countdown_timer/Clock.py
@@ -3,6 +3,16 @@ import pygame
 import msvcrt
 import customtkinter
 
+def update_clock():
+    current_time=time.strftime("%H:%M:%S")
+    time_label.configure(text=current_time)
+    app.after(1000,update_clock)
+
+def set_alarm_action():
+    selected_hour=hour_menu.get()
+    selected_min=minute_menu.get()
+    print(f"Alarm set for {selected_hour}:{selected_min}")
+
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("dark-blue")
 app=customtkinter.CTk()
@@ -14,10 +24,7 @@ frame.pack(pady=20)
 
 time_label=customtkinter.CTkLabel(master=frame,text="",font=("Helvetica",50),text_color="#00BFFF")
 time_label.pack(pady=20,padx=20,anchor="center")
-def update_clock():
-    current_time=time.strftime("%H:%M:%S")
-    time_label.configure(text=current_time)
-    app.after(1000,update_clock)
+
 
 control_frame=customtkinter.CTkFrame(master=app)
 control_frame.pack(pady=10)
@@ -33,7 +40,7 @@ minute_menu=customtkinter.CTkComboBox(master=control_frame,values=minutes)
 minute_menu.pack(side="left", pady=10)
 minute_menu.set("- -")
 
-set_alarm=customtkinter.CTkButton(master=control_frame,corner_radius=15,text="Set Alarm")
+set_alarm=customtkinter.CTkButton(master=control_frame,corner_radius=15,text="Set Alarm",command=set_alarm_action)
 set_alarm.pack(side="bottom",pady=20)
 
 

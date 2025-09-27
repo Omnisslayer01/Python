38009a27020584b7ff17cb53b5eb9bd49e59fc6b
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2025-09-27 23:47:19 +0530

    added sound to alarm but alarm runs 1:05 min non stop

--- Countdown_timer/Clock.py
+++ Countdown_timer/Clock.py
@@ -2,16 +2,42 @@ import time
 import pygame
 import msvcrt
 import customtkinter
+import sys
+import threading
+
+
+alarm_time=""
+alarm_status=False
 
 def update_clock():
+    global alarm_status
+    global alarm_time
     current_time=time.strftime("%H:%M:%S")
     time_label.configure(text=current_time)
     app.after(1000,update_clock)
+    list=alarm_time.split(":")
+    print(list)
+    if current_time.split(":")[0]==list[0]:
+        if current_time.split(":")[1]==list[1]:
+            alarm_status=True
+            list=[]
+            alarm_sound()
+        
+
+def alarm_sound():
+    if alarm_status==True:
+        pygame.mixer.init()
+        alarm_sound=pygame.mixer.Sound(r"C:\Users\DELL\Downloads\mixkit-classic-alarm-995.wav")
+        alarm_sound.play()    
 
 def set_alarm_action():
     selected_hour=hour_menu.get()
     selected_min=minute_menu.get()
     print(f"Alarm set for {selected_hour}:{selected_min}")
+    global alarm_time
+    alarm_time=f"{selected_hour}:{selected_min}"  
+
+
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -34,11 +60,11 @@ minutes=[f"{i:02}"for i in range(0,60)]
 
 hour_menu=customtkinter.CTkComboBox(master=control_frame,values=hours)
 hour_menu.pack(side="left", pady=10)
-hour_menu.set("- -")
+hour_menu.set("--")
 
 minute_menu=customtkinter.CTkComboBox(master=control_frame,values=minutes)
 minute_menu.pack(side="left", pady=10)
-minute_menu.set("- -")
+minute_menu.set("--")
 
 set_alarm=customtkinter.CTkButton(master=control_frame,corner_radius=15,text="Set Alarm",command=set_alarm_action)
 set_alarm.pack(side="bottom",pady=20)

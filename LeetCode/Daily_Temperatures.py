c2c776bd02b055d21f55225ed3f6c1701d3d1460
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2025-12-03 13:44:46 +0530

    Solves the problem but is too slow

--- LeetCode/Daily_Temperatures.py
+++ LeetCode/Daily_Temperatures.py
@@ -1,27 +1,20 @@
 class Solution:
     def dailyTemperatures(self, temperatures):
         length=len(temperatures)
-        output_list=[0*(length-1)]
+        output_list=[0]*length
         for i in range(0,length):
             for j in range(i+1,length):
                 if temperatures[j]>temperatures[i]:
-                    output_list.append(j-i)
+                    output_list[i]=(j-i)
                     break
-                elif i>length-2 and j==length-1:
-                    output_list.append(0)
-                    continue
-                elif i==length-2 and j==length-1:
-                    output_list.append(0)
-                    continue
-                
                 else:
                     continue
             
-        if i==length-1:
-            output_list.append(0)
+        
 
         print(output_list)
                     
 
 Temp=Solution()
-Temp.dailyTemperatures([55,38,53,81,61,93,97,32,43,78])
+Temp.dailyTemperatures([73,74,75,71,69,72,76,73])
+
